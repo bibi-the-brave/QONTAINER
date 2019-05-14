278416b98334449496a6c8086289f96b3e4597bb
Andrea <andrea.favero96@gmail.com>
2019-05-14 15:26:43 +0200

    Sistemato problema svg
    Aggiunto file qrc contenente le immagini e aggiustata la loto grandezza all'interno dei bottoni della finestra principale

--- finestraprincipale.cpp
+++ finestraprincipale.cpp
@@ -1,24 +1,27 @@
 #include "finestraprincipale.h"
-#include <QDir>
+#include <QIcon>
+#include <QPixmap>
 
 FinestraPrincipale::FinestraPrincipale(QWidget *parent)
     : QMainWindow(parent)
 {
     layout = new QHBoxLayout();
 
-    bInserAtleta = new QPushButton("nuovo atleta");
-    bInserAtleta->setIcon(QIcon(QDir::currentPath() + "/immagini/atleti.svg"));
+    bInserAtleta = new QPushButton(/*"nuovo atleta"*/);
+    bInserAtleta->setIcon(QIcon(QPixmap(":/immagini/atleti.svg")));
+    bInserAtleta->setIconSize(QSize(340,340));
     bInserAtleta->setToolTip("Creazione di un nuovo atleta");
     bInserAtleta->setFixedSize(QSize(350,350));
 
-    bInserAllenamento = new QPushButton("nuovo allenamento");
-    bInserAllenamento->setIcon(QIcon(QDir::currentPath()
-                                     + "/immagini/allenamento.svg"));
+    bInserAllenamento = new QPushButton(/*"nuovo allenamento"*/);
+    bInserAllenamento->setIcon(QIcon( ":/immagini/allenamento.svg"));
+    bInserAllenamento->setIconSize(QSize(325,325));
     bInserAllenamento->setToolTip("Inserirmento di un nuovo allenamento");
     bInserAllenamento->setFixedSize(QSize(350,350));
 
-    bRicerca = new QPushButton("ricerca");
-    bRicerca->setIcon(QIcon(QDir::currentPath() + "/immagini/ricerca.svg"));
+    bRicerca = new QPushButton(/*"ricerca"*/);
+    bRicerca->setIcon(QIcon(":/immagini/ricerca.svg"));
+    bRicerca->setIconSize(QSize(325,325));
     bRicerca->setToolTip("Ricerca allenamenti relativi ad uno sport o ad un atleta");
     bRicerca->setFixedSize(QSize(350,350));
 
